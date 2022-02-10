916f9fbd8e54de6873cc654c3d69dbc57b10ea5a
Niki Dokovski <nikolai.dokovski@sap.com>
2022-02-10 07:00:32 +0100

    update kind cluster setup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 dir=$(dirname $0)
 
-echo "setting up cluster"
+echo "setting up kind cluster"
 
 if ! command -v kind &> /dev/null
 then
@@ -19,9 +19,15 @@ then
 fi
 
 echo "create cluster"
-kind create cluster --name kind --config ${dir}/cluster/kind-config.yaml
+kind create cluster --name kind --config ${dir}/kind-config.yaml
 
 kubectl cluster-info --context kind-kind
+
+IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kind-control-plane)
+sudo ifconfig lo0 alias $IP
+
+kind get nodes | xargs -I{} docker exec -t {} sysctl fs.inotify.max_user_instances=1500
+
 
 for var in "$@"
 do
@@ -30,4 +36,9 @@ do
 		kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 		kubectl wait --for=condition=ready pod -l app.kubernetes.io/component=controller -n ingress-nginx --timeout=15m
 	fi
-done		
+
+	if [[ "$var" = "--add-net-alias" ]]; then
+        IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kind-control-plane)
+        sudo ifconfig lo0 alias $IP
+    done
+done
